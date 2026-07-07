c48a11bc9ac286e62f0d4e1d448d50e55e7bb9d8
Gabriel de França <gabriel@beeliked.com>
2026-07-07 15:13:17 -0300

    feat: config.js adjusted

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,18 +1,24 @@
 // Set API parameters
-export const API_URL = "http://localhost:8099/v2";
-export const CLIENT_ID = "7ff1d5e1-2ea2-4ccd-9ce9-dec34e62f53c";
-export const CLIENT_SECRET = "BDxnandBn2NTZzJ0jMWZp1z6kjft6U";
+export const API_URL = "https://api.beeliked.com/v2";
+export const CLIENT_ID = "xxx...";
+export const CLIENT_SECRET = "xxx...";
 
 // Set script parameters
 export const PLUGIN_SCRIPT_URL =
-  "https://reward-plugin.beeliked.local:3030/rewards-plugin.js";
-export const DATA_APP_UUID = "44f15880-e2a7-44d5-ab8e-4fa3e4b10494";
+  "https://rewards.beeliked.app/rewards-plugin.js";
+export const DATA_APP_UUID = "xxx...";
+
+// User info
+// NOTE: These values should be dynamic based on the user's account.
+// Currently, static values are used here only for testing purposes.
+// In a real-world scenario, `UID` and `DATA_NAME` would be retrieved from the logged-in user's account information.
+export const UID = "develoer@beeliked.com";
+export const DATA_NAME = "Developer"; // User name that will be displayed in the plugin
 
 // Embed configuration
 // Change only "type" to switch between embed modes: 'popup' | 'slide' | 'standard'
 export const EMBED_STYLE_CONFIG = {
-  type: "slide",
-
+  type: "popup",
   popup: {
     width: "1000px",
     height: "80vh",
@@ -31,10 +37,3 @@ export const EMBED_STYLE_CONFIG = {
     height: "600px",
   },
 };
-
-// User info
-// NOTE: These values should be dynamic based on the user's account.
-// Currently, static values are used here only for testing purposes.
-// In a real-world scenario, `UID` and `DATA_NAME` would be retrieved from the logged-in user's account information.
-export const UID = "gabriel@beeliked.com";
-export const DATA_NAME = "Gabriel";
